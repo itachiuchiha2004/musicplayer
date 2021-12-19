343933c4924e828453ce21630e2c65a324d22363
itachiuchiha2004 <96373609+itachiuchiha2004@users.noreply.github.com>
2021-12-19 16:39:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-from tkinter import ttk
-import tkinter
-from tkinter.ttk import *
+#importing neccessary libraries
 from tkinter import *
 import random as r
 from pygame import  mixer
@@ -14,23 +12,27 @@ from tkinter import messagebox
 #api id 
 # AIzaSyDo_8GZVuy35Si0pZySV3QZjQfIbT_wsHQ
 
-
+#creating a class
 class musixplayer():
+    #initializing 
     mixer.init()
     pygame.init()
 
     def __init__(self,window):
+        #variable for scale
         v1 = DoubleVar()
         window.title("musix player")
         window.geometry('500x350')
         window.resizable(0,0)
+        #buttons
         Play = Button(window,text='►',width=10,bg='red',font=('Times'),command=self.play_method).pack(side="top")
         Pause = Button(window,text='║║',width=10,bg='blue',font=('Times'),command=self.pause_method).pack(padx=2,pady=8)
         unPause = Button(window,text='unpause',width=10,bg='green',font=('Times'),command=self.unpause_method).pack(padx=2,pady=9)
         Open = Button(window,text='open',width=10,bg='yellow',font=('Times'),command=self.open).pack(padx=2,pady=10)
         Lyrics = Button(window,text='lyrics',width=10,bg='orange',font=('Times'),command=self.get_lyrics).pack(padx=2,pady=11)
+        #scale
         vol = Scale(root,variable=v1,from_=10,to=1,orient=VERTICAL,command=self.voladj).pack(anchor="nw")
-    
+    #methods for buttons
     def play_method(self):
         try:
             mixer.music.play()
@@ -49,14 +51,11 @@ class musixplayer():
     
     def get_lyrics(self):
         pass
-
-
-
+    
     def voladj(self,val):
         mixer.music.set_volume((float(val))/10)
 
-
-root = Tk()
-
-app = musixplayer(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = Tk()
+    app = musixplayer(root)
+    root.mainloop()
